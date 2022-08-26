86b47516ecb0b89e5e56d38f251b4bfeb13c617d
VeluruSumanvsn <velurusuman.vsn@gmail.com>
2022-08-26 08:21:23 +0530

    contact us page updated

--- src/Cart/ContactUs.js
+++ src/Cart/ContactUs.js
@@ -4,7 +4,8 @@ export const ContactUs = () => {
   return (
     <div>
         <h2>This is ContactUs page</h2>
-        <h3>This is an ecxampl demo</h3>
+        <h3>This is an ecxampl democxbbjbz fsajbcjknk</h3>
+        <h3>This is an ecxampl democxbbjbz zfas</h3>
         <Link to='/'>Back to Home Page</Link>
 
     </div>
